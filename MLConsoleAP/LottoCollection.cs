6bbd2804610664df9cbef5751412fe3289523ab9
Peter Liang <peter@jxtech.io>
2023-04-08 21:19:22 +0800

    first add

---
+++ MLConsoleAP/LottoCollection.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace MLConsoleAP
+{
+    public class LotteryData
+    {
+        [LoadColumn(0)]
+        public float Number1 { get; set; }
+
+        [LoadColumn(1)]
+        public float Number2 { get; set; }
+
+        [LoadColumn(2)]
+        public float Number3 { get; set; }
+
+        [LoadColumn(3)]
+        public float Number4 { get; set; }
+
+        [LoadColumn(4)]
+        public float Number5 { get; set; }
+
+        [LoadColumn(5)]
+        public float Number6 { get; set; }
+
+        [LoadColumn(6)]
+        [ColumnName("Label")]
+        public float SpecialNumber { get; set; } = -1.5f;
+
+        public override string ToString()
+        {
+            string result = $"Lotto Result: {Number1}, {Number2}, {Number3}, {Number4}, {Number5}, {Number6}, {SpecialNumber}";
+            return result;
+        }
+    }
+
+    public class LottoTool
+    {
+        public void TrainData()
+        {
+            var mlContext = new MLContext();
+            IDataView dataView = mlContext.Data.LoadFromTextFile<LotteryData>("lotto.txt", separatorChar: ',', hasHeader: true);
+
+            List<int[]> numbers = new List<int[]>();
+            //List<LotteryData> lotteryDatas = new List<LotteryData>();
+            //List<int> specialNumbers = new List<int>();
+
+            foreach (var row in mlContext.Data.CreateEnumerable<LotteryData>(dataView, reuseRowObject: true))
+            {
+                var numberArray = new int[] { (int)row.Number1, (int)row.Number2, (int)row.Number3, (int)row.Number4, (int)row.Number5, (int)row.Number6 };
+                numbers.Add(numberArray);
+                //specialNumbers.Add((int)row.SpecialNumber);
+                //lotteryDatas.Add(row);
+            }
+
+            //var data = mlContext.Data.LoadFromEnumerable(numbers.Select(x => new { Numbers = x }));
+            //var data = mlContext.Data.LoadFromEnumerable(lotteryDatas);
+            //var trainTestData = mlContext.Data.TrainTestSplit(data, testFraction: 0.2);
+            var trainTestData = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+
+            var pipeline = mlContext.Transforms.Conversion
+                                    .MapValueToKey(outputColumnName: "LabelAsKey", inputColumnName: "Label")
+                                    //.MapValueToKey("Label", keyData: lotteryDatas.ToArray())
+                                    .Append(mlContext.Transforms.Concatenate("Features", nameof(LotteryData.Number1), nameof(LotteryData.Number2), nameof(LotteryData.Number3), nameof(LotteryData.Number4), nameof(LotteryData.Number5), nameof(LotteryData.Number6)))
+                                    .Append(mlContext.Transforms.NormalizeMinMax("Features"))
+                                    //.Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"))
+                                    .Append(mlContext.Regression.Trainers.Sdca());
+                                    //.Append(mlContext.Regression.Trainers.FastTree());
+
+            //var model = pipeline.Fit(trainTestData.TrainSet);
+            var model = pipeline.Fit(dataView);
+
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<LotteryData, LotteryData>(model);
+
+            var lastNumbers = numbers.Last();
+            LotteryData lastData = new LotteryData
+            {
+                Number1 = lastNumbers[0],
+                Number2 = lastNumbers[1],
+                Number3 = lastNumbers[2],
+                Number4 = lastNumbers[3],
+                Number5 = lastNumbers[4],
+                Number6 = lastNumbers[5],
+                SpecialNumber = 0,
+            };
+            var predictedSpecialNumber = predictionEngine.Predict(lastData);
+            Console.WriteLine(predictedSpecialNumber);
+        }
+    }
+}
---
+++ MLConsoleAP/ShuffleRows.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML;
+
+namespace MLConsoleAP
+{
+    public static class ShuffleRows
+    {
+        public static void LottoDemo()
+        {
+            var mlContext = new MLContext();
+            var listData = GetLottoData();
+            var data = mlContext.Data.LoadFromEnumerable(listData);
+            var shuffledData = mlContext.Data.ShuffleRows(data, seed: 123);
+            var enumerable = mlContext.Data
+                .CreateEnumerable<LottoData>(shuffledData,
+                reuseRowObject: true);
+            foreach (var row in enumerable) 
+            {
+                Console.WriteLine($"date: {row.Date.ToString()}, number: {row.ToString()}");
+            }
+
+        }
+
+        public static int[] GetRandomNumbers()
+        {
+            List<int> numbers = new List<int>();
+            var random = new Random();
+            numbers.Add(random.Next(1, 50));
+            numbers.Add(random.Next(1, 50));
+            numbers.Add(random.Next(1, 50));
+            numbers.Add(random.Next(1, 50));
+            numbers.Add(random.Next(1, 50));
+            numbers.Add(random.Next(1, 50));
+
+            return numbers.ToArray();
+        }
+
+        public static int[] GetRandomNumbers(int length,int start,int end)
+        {
+            List<int> numbers = new List<int>();
+            var random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int number = random.Next(start, end);
+                while(numbers.Contains(number))
+                {
+                    number = random.Next(start, end);
+                }
+                numbers.Add(number);
+            }
+
+            return numbers.ToArray();
+        }
+
+        public static void PrintRandomNumbers()
+        {
+            var numbers = GetRandomNumbers(6, 1, 50);
+            string str = string.Empty;
+            foreach (var num in numbers)
+            {
+                str += num.ToString() + " ";
+            }
+            Console.WriteLine(str);
+        }
+
+        public static void PrintRandomNumbers2()
+        {
+            var numbers = GetRandomNumbers(12, 1, 25);
+            string str = string.Empty;
+            foreach (var num in numbers)
+            {
+                str += num.ToString() + " ";
+            }
+            Console.WriteLine(str);
+        }
+
+        // Sample class showing how to shuffle rows in 
+        // IDataView.
+        public static void Example()
+        {
+            // Create a new context for ML.NET operations. It can be used for
+            // exception tracking and logging, as a catalog of available operations
+            // and as the source of randomness.
+            var mlContext = new MLContext();
+
+            // Get a small dataset as an IEnumerable.
+            var enumerableOfData = GetSampleTemperatureData(5);
+            var data = mlContext.Data.LoadFromEnumerable(enumerableOfData);
+
+            // Before we apply a filter, examine all the records in the dataset.
+            Console.WriteLine($"Date\tTemperature");
+            foreach (var row in enumerableOfData)
+            {
+                Console.WriteLine($"{row.Date.ToString("d")}" +
+                    $"\t{row.Temperature}");
+            }
+            Console.WriteLine();
+            // Expected output:
+            //  Date    Temperature
+            //  1/2/2012        36
+            //  1/3/2012        36
+            //  1/4/2012        34
+            //  1/5/2012        35
+            //  1/6/2012        35
+
+            // Shuffle the dataset.
+            var shuffledData = mlContext.Data.ShuffleRows(data, seed: 123);
+
+            // Look at the shuffled data and observe that the rows are in a
+            // randomized order.
+            var enumerable = mlContext.Data
+                .CreateEnumerable<SampleTemperatureData>(shuffledData,
+                reuseRowObject: true);
+
+            Console.WriteLine($"Date\tTemperature");
+            foreach (var row in enumerable)
+            {
+                Console.WriteLine($"{row.Date.ToString("d")}" +
+                $"\t{row.Temperature}");
+            }
+            // Expected output:
+            //  Date    Temperature
+            //  1/4/2012        34
+            //  1/2/2012        36
+            //  1/5/2012        35
+            //  1/3/2012        36
+            //  1/6/2012        35
+        }
+
+        private class SampleTemperatureData
+        {
+            public DateOnly Date { get; set; }
+            public float Temperature { get; set; }
+        }
+
+        public class LottoData
+        {
+            public DateTime Date { get; set; } = DateTime.Now;
+            public int[]? Numbers { get; set; } = null;
+
+            public override string ToString()
+            {
+                string str = string.Empty;
+                foreach (var num in Numbers)
+                {
+                    str += num.ToString() + " ";
+                }
+                return str;
+            }
+        }
+
+        /// <summary>
+        /// Get a fake temperature dataset.
+        /// </summary>
+        /// <param name="exampleCount">The number of examples to return.</param>
+        /// <returns>An enumerable of <see cref="SampleTemperatureData"/>.</returns>
+        private static IEnumerable<SampleTemperatureData> GetSampleTemperatureData(
+            int exampleCount)
+
+        {
+            var rng = new Random(1234321);
+            var date = new DateOnly(2012, 1, 1);
+            float temperature = 39.0f;
+
+            for (int i = 0; i < exampleCount; i++)
+            {
+                date = date.AddDays(1);
+                temperature += rng.Next(-5, 5);
+                yield return new SampleTemperatureData
+                {
+                    Date = date,
+                    Temperature =
+                    temperature
+                };
+
+            }
+        }
+
+        public static List<LottoData> GetLottoData()
+        {
+            List<LottoData> list = new List<LottoData>();
+            list.Add(new LottoData
+            {
+                Date = new DateTime(2023, 3, 7),
+                Numbers = new int[] { 13, 17, 23, 25, 32, 41 },
+            });
+            list.Add(new LottoData
+            {
+                Date = new DateTime(2023, 3, 3),
+                Numbers = new int[] { 13, 16, 18, 22, 34, 42 },
+            });
+            list.Add(new LottoData
+            {
+                Date = new DateTime(2023, 2, 28),
+                Numbers = new int[] { 02, 11, 25, 26, 27, 35 },
+            });
+            list.Add(new LottoData
+            {
+                Date = new DateTime(2023, 2, 24),
+                Numbers = new int[] { 15, 38, 39, 40, 45, 48 },
+            });
+            list.Add(new LottoData
+            {
+                Date = new DateTime(2023, 2, 21),
+                Numbers = new int[] { 01, 05, 06, 11, 26, 46 },
+            });
+            list.Add(new LottoData
+            {
+                Date = new DateTime(2023, 2, 17),
+                Numbers = new int[] { 07, 09, 14, 16, 26, 33 },
+            });
+
+            return list;
+        }
+    }
+}
